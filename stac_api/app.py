16f2d71bcd01add3e2f7d8679ef413082ec7c5cf
Alex Leith <alexgleith@gmail.com>
2023-10-04 07:00:07 +1100

    Update id, title and description

--- stac_api/app.py
+++ stac_api/app.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from stac_fastapi.api.app import StacApi
@@ -16,6 +18,8 @@ from stac_fastapi.pgstac.db import close_db_connection, connect_to_db
 from stac_fastapi.pgstac.types.search import PgstacSearch
 from starlette.middleware.cors import CORSMiddleware
 
+ENVIRONMENT = os.environ.get("ENVIRONMENT", "staging")
+
 settings = Settings()
 
 extensions = [
@@ -29,12 +33,20 @@ extensions = [
 post_request_model = create_post_request_model(extensions, base_model=PgstacSearch)
 get_request_model = create_get_request_model(extensions)
 
+id = "dep-stac"
+title = "Digital Earth Pacific STAC API"
+description = "A SpatioTemporal Asset Catalog (STAC) API for Digital Earth Pacific"
+if ENVIRONMENT != "production":
+    id = id + f"-{ENVIRONMENT.lower()}"
+    title = title + f" ({ENVIRONMENT})"
+    description = description + f" ({ENVIRONMENT})"
+
 api = StacApi(
-    title="Digital Earth Pacific STAC API",
-    description="A SpatioTemporal Asset Catalog (STAC) API for Digital Earth Pacific",
+    title=title,
+    description=description,
     extensions=[ContextExtension()],
     settings=settings,
-    client=CoreCrudClient(post_request_model=post_request_model),
+    client=CoreCrudClient(post_request_model=post_request_model, landing_page_id=id),
     search_get_request_model=get_request_model,
     response_class=ORJSONResponse,
 )
