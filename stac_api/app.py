318d9ab507dcd0efdaf7b7d1b87843677112bc68
Alex Leith <alexgleith@gmail.com>
2023-10-11 16:41:02 +1100

    Fix search issue

--- stac_api/app.py
+++ stac_api/app.py
@@ -20,6 +20,14 @@ from starlette.middleware.cors import CORSMiddleware
 
 ENVIRONMENT = os.environ.get("ENVIRONMENT", "staging")
 
+id = "dep-stac"
+title = "Digital Earth Pacific STAC API"
+description = "A SpatioTemporal Asset Catalog (STAC) API for Digital Earth Pacific"
+if ENVIRONMENT != "production":
+    id = id + f"-{ENVIRONMENT.lower()}"
+    title = title + f" ({ENVIRONMENT})"
+    description = description + f" ({ENVIRONMENT})"
+
 settings = Settings()
 
 extensions = [
@@ -33,21 +41,14 @@ extensions = [
 post_request_model = create_post_request_model(extensions, base_model=PgstacSearch)
 get_request_model = create_get_request_model(extensions)
 
-id = "dep-stac"
-title = "Digital Earth Pacific STAC API"
-description = "A SpatioTemporal Asset Catalog (STAC) API for Digital Earth Pacific"
-if ENVIRONMENT != "production":
-    id = id + f"-{ENVIRONMENT.lower()}"
-    title = title + f" ({ENVIRONMENT})"
-    description = description + f" ({ENVIRONMENT})"
-
 api = StacApi(
     title=title,
     description=description,
-    extensions=[ContextExtension()],
+    extensions=extensions,
     settings=settings,
     client=CoreCrudClient(post_request_model=post_request_model, landing_page_id=id),
     search_get_request_model=get_request_model,
+    search_post_request_model=post_request_model,
     response_class=ORJSONResponse,
 )
 
