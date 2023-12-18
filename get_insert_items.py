fb90e55487d9530e3ff27980f06e901ee2b64f2d
Alex Leith <alexgleith@gmail.com>
2023-12-18 11:04:36 +1100

    Switch to using pystac to grab items

--- get_insert_items.py
+++ get_insert_items.py
@@ -1,36 +1,50 @@
 #!/usr/bin/env python3
 
 import json
+from multiprocessing.dummy import Pool as ThreadPool
+from typing import Iterable
 
 import click
 import pystac
-from dep_tools.azure import download_blobs, list_blob_container
+from dep_tools.azure import list_blob_container
 from dep_tools.utils import get_container_client
 from pypgstac.db import PgstacDB
 from pypgstac.load import Loader, Methods
 from pystac import Item
 
 
+def download_blob_item(blob: str):
+    url = f"https://deppcpublicstorage.blob.core.windows.net/output/{blob}"
+    return Item.from_file(url)
+
+
+def download_blob_items(stac_urls: list[str]) -> Iterable[Item]:
+    pool = ThreadPool(20)
+    items = pool.map(download_blob_item, stac_urls)
+    pool.close()
+    pool.join()
+
+    return items
+
+
 def insert_items(
-    items: list, collection_override: str = None, dump_items: bool = False
+    items: Iterable[Item], collection_override: str = None, dump_items: bool = False
 ) -> int:
     all_items = []
 
-    for item_bytes in items:
-        item = json.loads(item_bytes)
+    for item in items:
         if collection_override is not None:
-            stac_item = Item.from_dict(item)
-            stac_item.collection_id = collection_override
-            stac_item.add_link(
+            item.collection_id = collection_override
+            item.add_link(
                 pystac.Link(
                     pystac.RelType.COLLECTION,
                     collection_override,
                     media_type=pystac.MediaType.JSON,
                 )
             )
-            item = stac_item.to_dict()
+            item = item.to_dict()
 
-        all_items.append(item)
+        all_items.append(item.to_dict())
 
     if dump_items:
         with open("items.json", "w") as f:
@@ -58,7 +72,7 @@ def get_insert_items(
 ):
     container_client = get_container_client(container_name=storage_container)
     stac_urls = list_blob_container(container_client, prefix, suffix=".stac-item.json")
-    items = download_blobs(container_client, stac_urls)
+    items = download_blob_items(stac_urls)
     count = insert_items(items, collection_override, dump_items=dump_items)
     print(f"Found and updated {count} items in the database")
 
