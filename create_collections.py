29ed80df7a37aca8b50131b5af1002059ce6d5bf
Jesse Anderson <jesse.anderson@gmail.com>
2025-03-03 16:21:45 -0700

    add wofs all time summary

--- create_collections.py
+++ create_collections.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from dep_collections.dep_ls_geomad import dep_ls_geomad
 from dep_collections.dep_ls_wofs_summary_annual import dep_ls_wofs_summary_annual
+from dep_collections.dep_ls_wofs_summary_alltime import dep_ls_wofs_summary_alltime
 from dep_collections.dep_ls_wofl import dep_ls_wofl
 from dep_collections.dep_s1_geomad import dep_s1_geomad
 from dep_collections.dep_s2_geomad import dep_s2_geomad
@@ -19,13 +20,14 @@ out_dir.mkdir(exist_ok=True)
 
 all_collections = (
     dep_ls_wofs_summary_annual,
+    dep_ls_wofs_summary_alltime,
     dep_ls_wofl,
     dep_ls_geomad,
     dep_s1_geomad,
     dep_s2_geomad,
     dep_s2_mangroves,
     dep_s2s1_mrd,
-    dep_s2ls_intertidal
+    dep_s2ls_intertidal,
 )
 
 for collection in all_collections:
